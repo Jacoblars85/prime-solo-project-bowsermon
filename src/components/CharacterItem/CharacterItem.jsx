2024491ad29dee01dabde620516967ec386c0ddb
Jacob Larson <hockeybobber@comcast.net>
2024-02-23 17:16:49 -0500

    formated character item

--- src/components/CharacterItem/CharacterItem.jsx
+++ src/components/CharacterItem/CharacterItem.jsx
@@ -1,18 +1,17 @@
-import { useState, Fragment, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-
+import { useState, Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function CharacterItem({ character }) {
-  const [isPicture, setIsPicture] = useState(false)
+  const [isPicture, setIsPicture] = useState(false);
 
-  const user = useSelector(store => store.user.userReducer);
-  const starter = useSelector(store => store.character.starter);
+  const user = useSelector((store) => store.user.userReducer);
+  const starter = useSelector((store) => store.character.starter);
 
   const dispatch = useDispatch();
 
@@ -23,223 +22,207 @@ function CharacterItem({ character }) {
   };
 
   const setStarterOne = () => {
-
     if (starter.length === 0) {
       dispatch({
-        type: 'SAGA_SET_STARTER_ONE',
-        payload: character.id
+        type: "SAGA_SET_STARTER_ONE",
+        payload: character.id,
       });
     } else if (starter.length === 1) {
-
       if (character.starter_2 === true) {
         dispatch({
-          type: 'SAGA_SET_STARTER_CONDITIONALLY',
+          type: "SAGA_SET_STARTER_CONDITIONALLY",
           payload: {
             characterId: character.id,
             currentStarter: 1,
-            otherStarter: 2
-          }
+            otherStarter: 2,
+          },
         });
-
       } else {
         dispatch({
-          type: 'SAGA_SET_STARTER_ONE',
-          payload: character.id
+          type: "SAGA_SET_STARTER_ONE",
+          payload: character.id,
         });
       }
-
     } else if (starter.length === 2) {
       if (character.id === starter[1].id) {
-
         dispatch({
-          type: 'SAGA_SET_STARTER_CONDITIONALLY',
+          type: "SAGA_SET_STARTER_CONDITIONALLY",
           payload: {
             characterId: character.id,
             currentStarter: 1,
-            otherStarter: 2
-          }
+            otherStarter: 2,
+          },
         });
-
       } else {
         dispatch({
-          type: 'SAGA_SET_STARTER_ONE',
-          payload: character.id
+          type: "SAGA_SET_STARTER_ONE",
+          payload: character.id,
         });
       }
     }
-  }
+  };
 
   const setStarterTwo = () => {
     if (starter.length === 0) {
       dispatch({
-        type: 'SAGA_SET_STARTER_TWO',
-        payload: character.id
+        type: "SAGA_SET_STARTER_TWO",
+        payload: character.id,
       });
     } else if (starter.length === 1) {
-
       if (character.starter_1 === true) {
         dispatch({
-          type: 'SAGA_SET_STARTER_CONDITIONALLY',
+          type: "SAGA_SET_STARTER_CONDITIONALLY",
           payload: {
             characterId: character.id,
             currentStarter: 2,
-            otherStarter: 1
-          }
+            otherStarter: 1,
+          },
         });
       } else {
         dispatch({
-          type: 'SAGA_SET_STARTER_TWO',
-          payload: character.id
+          type: "SAGA_SET_STARTER_TWO",
+          payload: character.id,
         });
       }
     } else if (starter.length === 2) {
       if (character.id === starter[0].id) {
         dispatch({
-          type: 'SAGA_SET_STARTER_CONDITIONALLY',
+          type: "SAGA_SET_STARTER_CONDITIONALLY",
           payload: {
             characterId: character.id,
             currentStarter: 2,
-            otherStarter: 1
-          }
+            otherStarter: 1,
+          },
         });
       } else {
         dispatch({
-          type: 'SAGA_SET_STARTER_TWO',
-          payload: character.id
+          type: "SAGA_SET_STARTER_TWO",
+          payload: character.id,
         });
       }
     }
-
-  }
+  };
 
   const clearStarter = () => {
-
     dispatch({
-      type: 'SAGA_CLEAR_STARTER',
-      payload: character.id
+      type: "SAGA_CLEAR_STARTER",
+      payload: character.id,
     });
-  }
-
-
+  };
 
   const sellCharacter = () => {
     if (starter.length === 2) {
       if (character.id === starter[0].id || character.id === starter[1].id) {
         setOpen(false);
-        return alert("you can't sell your starter")
+        return alert("you can't sell your starter");
       } else {
         dispatch({
-          type: 'SAGA_SELL_CHARACTER',
+          type: "SAGA_SELL_CHARACTER",
           payload: {
-            characterID: character.id
-          }
-        })
+            characterID: character.id,
+          },
+        });
       }
-
     } else if (starter.length === 1) {
       if (character.id === starter[0].id) {
         setOpen(false);
-        return alert("you can't sell your starter")
+        return alert("you can't sell your starter");
       } else {
         dispatch({
-          type: 'SAGA_SELL_CHARACTER',
+          type: "SAGA_SELL_CHARACTER",
           payload: {
-            characterID: character.id
-          }
-        })
+            characterID: character.id,
+          },
+        });
       }
     } else {
       dispatch({
-        type: 'SAGA_SELL_CHARACTER',
+        type: "SAGA_SELL_CHARACTER",
         payload: {
-          characterID: character.id
-        }
-      })
+          characterID: character.id,
+        },
+      });
     }
     setOpen(false);
-  }
+  };
 
   const confirmSale = () => {
     setOpen(true);
-
-  }
+  };
 
   const togglePicture = () => {
-    setIsPicture(!isPicture)
-  }
+    setIsPicture(!isPicture);
+  };
 
   const toggleNewClass = () => {
-
     dispatch({
-      type: 'SAGA_SET_OLD',
-      payload: character.id
+      type: "SAGA_SET_OLD",
+      payload: character.id,
     });
-  }
+  };
 
-  const doNothing = () => {
-
-  }
+  const doNothing = () => {};
 
   const displayText = () => {
     if (isPicture) {
       return (
-        <div className='descriptionForCharacterSingle'>
-
-          <div className='mainStatText'>
-
-            <p className='characterTextTotalHp'>{character.hp} hp</p>
-            <p className='characterTextStamina'>{character.stamina} stamina</p>
-            <p className='characterTextSpeed'>{character.speed} speed</p>
-
+        <div className="descriptionForCharacterSingle">
+          <div className="mainStatText">
+            <p className="characterTextTotalHp">{character.hp} hp</p>
+            <p className="characterTextStamina">{character.stamina} stamina</p>
+            <p className="characterTextSpeed">{character.speed} speed</p>
           </div>
 
-          <div className='attackStatText'>
-            <p className='characterTextBoxAttack'>{character.unique_attack}</p>
-            <p className='characterTextHp'>{character.unique_damage} damage</p>
-            <p className='characterTextStamina'>{character.unique_stamina} stamina</p>
+          <div className="attackStatText">
+            <p className="characterTextBoxAttack">{character.unique_attack}</p>
+            <p className="characterTextHp">{character.unique_damage} damage</p>
+            <p className="characterTextStamina">
+              {character.unique_stamina} stamina
+            </p>
           </div>
-
         </div>
-      )
+      );
     } else {
       return (
-        <div className='characterPictures'>
+        <div className="characterPictures">
           <img src={character.profile_pic} />
         </div>
-      )
+      );
     }
-  }
+  };
 
   return (
-    <div className={character.new ? "new" : "single-box"} onMouseOver={character.new ? toggleNewClass : doNothing}>
-
-      <div className='starterPTag'>
-
+    <div
+      className={character.new ? "new" : "single-box"}
+      onMouseOver={character.new ? toggleNewClass : doNothing}
+    >
+      <div className="starterPTag">
         <p>{character.new ? "new" : character.starter_1 ? "Starter 1" : ""}</p>
 
         <p>{character.starter_2 ? "Starter 2" : ""}</p>
-
       </div>
 
       <h5>{character.name}</h5>
 
-      <ul className='singleBoxUl' onClick={togglePicture}> {displayText()} </ul>
+      <ul className="singleBoxUl" onClick={togglePicture}>
+        {" "}
+        {displayText()}{" "}
+      </ul>
 
-      <div className='buttonBox'>
-
-        <button id={character.id} onClick={setStarterOne} >Set Starter 1</button>
-        <button id={character.id} onClick={setStarterTwo} >Set Starter 2</button>
-        <button id={character.id} onClick={clearStarter} >Remove</button>
-        <button id={character.id} onClick={confirmSale} >Sell</button>
-
+      <div className="buttonBox">
+        <button id={character.id} onClick={setStarterOne}>
+          Set Starter 1
+        </button>
+        <button id={character.id} onClick={setStarterTwo}>
+          Set Starter 2
+        </button>
+        <button id={character.id} onClick={clearStarter}>
+          Remove
+        </button>
+        <button id={character.id} onClick={confirmSale}>
+          Sell
+        </button>
       </div>
-
-
-      {/* <p>{character.new ? "new" : ""}</p> */}
-
-
-
-
 
       <Fragment>
         <Dialog
@@ -248,26 +231,49 @@ function CharacterItem({ character }) {
           aria-labelledby="alert-dialog-title"
           aria-describedby="alert-dialog-description"
         >
-          <DialogTitle id="alert-dialog-title" sx={{ fontFamily: 'New Super Mario Font U', textAlign: 'center' }}>
+          <DialogTitle
+            id="alert-dialog-title"
+            sx={{ fontFamily: "New Super Mario Font U", textAlign: "center" }}
+          >
             {`Are you sure you want to sell ${character.name}`}
           </DialogTitle>
           <DialogContent>
-            <DialogContentText id="alert-dialog-description" sx={{ fontFamily: 'New Super Mario Font U', textAlign: 'center' }}>
-              You will receive 10 coins if you sell {character.name}. You will have to buy the new character box to have the chance to get {character.name} back.
+            <DialogContentText
+              id="alert-dialog-description"
+              sx={{ fontFamily: "New Super Mario Font U", textAlign: "center" }}
+            >
+              You will receive 10 coins if you sell {character.name}. You will
+              have to buy the new character box to have the chance to get{" "}
+              {character.name} back.
             </DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Button sx={{ color: 'black', fontSize: 20, fontFamily: 'New Super Mario Font U' }} onClick={sellCharacter} autoFocus>
+            <Button
+              sx={{
+                color: "black",
+                fontSize: 20,
+                fontFamily: "New Super Mario Font U",
+              }}
+              onClick={sellCharacter}
+              autoFocus
+            >
               Sell
             </Button>
-            <Button sx={{ color: 'black', fontSize: 20, fontFamily: 'New Super Mario Font U' }} onClick={handleClose}>Close</Button>
-
+            <Button
+              sx={{
+                color: "black",
+                fontSize: 20,
+                fontFamily: "New Super Mario Font U",
+              }}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
           </DialogActions>
         </Dialog>
       </Fragment>
-
     </div>
-  )
+  );
 }
 
 export default CharacterItem;
