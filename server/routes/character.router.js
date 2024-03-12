8048b60b4ba441450163e419e9415cbd87765952
Jacob Larson <hockeybobber@comcast.net>
2024-03-12 18:21:32 -0500

    added new sql for the get route for a character

--- server/routes/character.router.js
+++ server/routes/character.router.js
@@ -5,26 +5,37 @@ const pool = require('../modules/pool')
 router.get('/character', (req, res) => {
     // console.log('im in character get');
     const query = `
-  SELECT "user_characters"."id" as "id",
-		"user_characters"."user_id" as "user_id",
-		"user_characters"."character_id",
-        "user_characters"."starter_1",
-        "user_characters"."starter_2",
-        "user_characters"."new",
-		"characters"."name",
-		"characters"."profile_pic",
-		"characters"."hp",
-		"characters"."stamina",
-        "characters"."speed",
-		"characters"."unique_attack",
-		"characters"."unique_damage",
-		"characters"."unique_stamina",
-        "characters"."battle_pic"
- FROM "user_characters"
-	INNER JOIN "characters"
-    	ON "user_characters"."character_id" = "characters"."id"
-    WHERE "user_id" = $1
-    ORDER BY "character_id", "id" DESC;
+    SELECT "user_characters"."id" as "id",
+    "user_characters"."user_id" as "user_id",
+    "user_characters"."character_id",
+    "user_characters"."starter_1",
+    "user_characters"."starter_2",
+    "user_characters"."new",
+    "characters"."name",
+    "characters"."profile_pic",
+    "characters"."hp",
+    "characters"."stamina",
+    "characters"."speed",
+    "characters"."unique_attack",
+    "characters"."unique_damage",
+    "characters"."unique_stamina",
+    "characters"."battle_pic",
+    "items"."name" as "item_name",
+    "items"."hp" as "item_hp",
+    "items"."stamina" as "item_stamina",
+    "items"."pic" as "item_pic",
+    "items"."type" as "item_type",
+    "items"."speed" as "item_speed",
+    "items"."attack" as "item_attack",
+    "items"."cost" as "item_cost",
+    "items"."color" as "item_color"
+FROM "user_characters" 
+INNER JOIN "characters"
+    ON "user_characters"."character_id" = "characters"."id"
+INNER JOIN "items"
+    ON "user_characters"."item" = "items"."id"
+WHERE "user_id" = $1
+ORDER BY "character_id", "id" DESC;
   `;
 
   const sqlValues = [req.user.id];
@@ -37,7 +48,6 @@ router.get('/character', (req, res) => {
             console.log('ERROR: Get all characters', err);
             res.sendStatus(500)
         })
-
 });
 
 
