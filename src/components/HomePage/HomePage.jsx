80428b3ac6b00300030d613b9bf71046936427b6
Jacob Larson <hockeybobber@comcast.net>
2024-03-15 16:46:48 -0500

    formatted home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,55 +1,67 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import './HomePage.css';
-import Nav from '../Nav/Nav';
-import StarterItem from '../StarterItem/StarterItem';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import "./HomePage.css";
+import Nav from "../Nav/Nav";
+import StarterItem from "../StarterItem/StarterItem";
 
 function HomePage() {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const user = useSelector((store) => store.user.userReducer);
-  const starter = useSelector(store => store.character.starter);
-
+  const starter = useSelector((store) => store.character.starter);
 
   useEffect(() => {
-    dispatch({ type: 'SAGA_FETCH_BATTLE_INFO' });
+    dispatch({ type: "SAGA_FETCH_BATTLE_INFO" });
   }, []);
 
   useEffect(() => {
-    dispatch({ type: 'SAGA_FETCH_IVENTORY' });
+    dispatch({ type: "SAGA_FETCH_IVENTORY" });
   }, []);
 
   return (
     <div>
-
       <Nav />
 
       <div className="homePage">
-
         <div className="starterHomeView">
-
-          {starter.map(start => {
+          {starter.map((start) => {
             return (
               <div className="starter-single-box" key={start.id}>
-                                  <h2>Starter {start.starter_1 === true ? "1" : start.starter_2 ? "2" : ""}</h2>
+                <h2>
+                  Starter{" "}
+                  {start.starter_1 === true ? "1" : start.starter_2 ? "2" : ""}
+                </h2>
                 <StarterItem key={start.id} start={start} />
               </div>
-
-            )
+            );
           })}
         </div>
 
+        <button
+          className="characterButton"
+          onClick={() => history.push(`/characters`)}
+        >
+          Characters
+        </button>
+        <button className="shopButton" onClick={() => history.push(`/shop`)}>
+          Shop
+        </button>
+        <button
+          className="inventoryButton"
+          onClick={() => history.push(`/inventory`)}
+        >
+          Inventory
+        </button>
 
-        <button className="characterButton" onClick={() => history.push(`/characters`)}>Characters</button>
-        <button className="shopButton" onClick={() => history.push(`/shop`)}>Shop</button>
-        <button className="inventoryButton" onClick={() => history.push(`/inventory`)}>Inventory</button>
-
-        <button className="campaignButton" onClick={() => history.push(`/campaign`)}>Campaign</button>
-
+        <button
+          className="campaignButton"
+          onClick={() => history.push(`/campaign`)}
+        >
+          Campaign
+        </button>
       </div>
-
     </div>
   );
 }
