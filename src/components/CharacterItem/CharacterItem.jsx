cee7f058c3ac17b347a6a481c58c20ae3718cc65
Jacob Larson <hockeybobber@comcast.net>
2024-03-16 17:52:51 -0500

    added more stuff in the form dialog

--- src/components/CharacterItem/CharacterItem.jsx
+++ src/components/CharacterItem/CharacterItem.jsx
@@ -224,14 +224,14 @@ function CharacterItem({ character }) {
   };
 
 
-  const [open, setOpen] = React.useState(false);
+  const [openNickname, setOpenNickname] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleClickOpenNickname = () => {
+    setOpenNickname(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleNicknameClose = () => {
+    setOpenNickname(false);
   };
 
   const editCharacterName = () => {
@@ -410,41 +410,40 @@ function CharacterItem({ character }) {
 
 
       <Dialog
-        open={open}
-        onClose={handleClose}
+        open={openNickname}
+        onClose={handleNicknameClose}
         PaperProps={{
           component: 'form',
           onSubmit: (event) => {
             event.preventDefault();
             const formData = new FormData(event.currentTarget);
             const formJson = Object.fromEntries(formData.entries());
-            const email = formJson.email;
-            console.log(email);
-            handleClose();
+            const newName = formJson.nickname;
+            console.log(newName);
+            handleNicknameClose();
           },
         }}
       >
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>Do you want to give {character.name} a nickname?</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
+            To give {character.name} a new name, fill out textfield below with the name you want to give it and click submit. You can always change it later.
           </DialogContentText>
           <TextField
             autoFocus
             required
             margin="dense"
             id="name"
-            name="email"
-            label="Email Address"
-            type="email"
+            name="nickname"
+            label="Nickname"
+            type="nickname"
             fullWidth
             variant="standard"
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
+        <Button type="submit">Submit</Button>
+          <Button onClick={handleNicknameClose}>Cancel</Button>
         </DialogActions>
       </Dialog>
 
@@ -466,7 +465,7 @@ function CharacterItem({ character }) {
               <CloseIcon />
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              {character.name} {" "}<EditIcon fontSize="small" onClick={editCharacterName}/>
+              {character.name} {" "}<EditIcon fontSize="small" onClick={handleClickOpenNickname}/>
             </Typography>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
               {character.starter_1 === true
