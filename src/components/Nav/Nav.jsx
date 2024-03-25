d4ff788086fbfb5ea3eee3a304575aa59f3ef337
Jacob Larson <hockeybobber@comcast.net>
2024-03-25 16:03:56 -0500

    added a level progress bar and it works

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,8 +5,54 @@ import './Nav.css';
 import { useSelector } from 'react-redux';
 import Settings from '../Settings/Settings';
 
-function Nav() {
+import PropTypes from 'prop-types';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
+function CircularProgressWithLabel(props) {
   const user = useSelector((store) => store.user.userReducer);
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography variant="caption" component="div" color="text.secondary">
+          {Math.floor(user.xp_level)}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+CircularProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate variant.
+   * Value between 0 and 100.
+   * @default 0
+   */
+  value: PropTypes.number.isRequired,
+};
+
+
+
+function Nav(props) {
+  const user = useSelector((store) => store.user.userReducer);
+
+  // console.log('user', user);
+
+  const normalise = () => ((user.xp_level - Math.floor(user.xp_level ) - 0) * 100) / (1 - 0);
 
   return (
     <div className="nav">
@@ -28,6 +74,8 @@ function Nav() {
           <>
             <span className='navLink'><img height={20} width={20} src='/images/Coin_-_New_Super_Mario_Bros.webp'/> {user.coins}</span>
 
+            <CircularProgressWithLabel value={normalise(props.value)} />
+
             <div className='navLink'>
               <Settings />
               </div>
@@ -44,3 +92,20 @@ function Nav() {
 }
 
 export default Nav;
+
+
+
+// export default function CircularWithValueLabel() {
+//   const [progress, setProgress] = React.useState(10);
+
+//   React.useEffect(() => {
+//     const timer = setInterval(() => {
+//       setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
+//     }, 800);
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+
+//   return <CircularProgressWithLabel value={progress} />;
+// }
