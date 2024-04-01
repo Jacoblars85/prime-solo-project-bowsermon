7128c6bebe92f1278ba630412189476ffd3472b4
Jacob Larson <hockeybobber@comcast.net>
2024-03-31 21:21:31 -0500

    added new fuction to tell about all boxes

--- src/components/MysteryBoxItem/MysteryBoxItem.jsx
+++ src/components/MysteryBoxItem/MysteryBoxItem.jsx
@@ -24,6 +24,7 @@ function MysteryBoxItem({ mysteryBoxItem }) {
 
   const user = useSelector((store) => store.user.userReducer);
   const characters = useSelector((store) => store.character.userCharacters);
+  const allCharacters = useSelector((store) => store.character.allCharacters);
 
   const [randomOpen, setRandomOpen] = useState(false);
 
@@ -39,32 +40,97 @@ function MysteryBoxItem({ mysteryBoxItem }) {
   const [newRewardName, setNewRewardName] = useState("");
   const [newRewardId, setNewRewardId] = useState();
 
+
   const openBox = (rewardId) => {
-    if (user.coins < 15) {
+    let randomNum;
+
+    if (user.coins < mysteryBoxItem.cost) {
       setRandomOpen(false);
       return alert("you are broke, sorry");
-    } else if (characters.length >= 20) {
-      setRandomOpen(false);
-      return alert("you can only have 20 characters");
     } else {
-      let randomNum = Math.floor(Math.random() * 9 + 1);
 
-      const newCharacter = allCharacters.find(
-        (Characters) => Characters.id === randomNum
-      );
+    if (mysteryBoxItem.id === 1) {
+      if (characters.length >= 20) {
+        return alert("you can only have 20 characters");
+      } else {
+        // character box
+        randomNum = Math.floor(Math.random() * 9 + 1);
 
-      setNewRewardPic("images/mysteryBoxGif.gif");
+        const newCharacter = allCharacters.find(
+          (Characters) => Characters.id === randomNum
+        );
+
+        setNewRewardPic("images/mysteryBoxGif.gif");
+        setNewRewardName("...");
+
+        setTimeout(() => {
+          setNewRewardPic(newCharacter.profile_pic);
+          setNewRewardName(newCharacter.name);
+        }, 2500);
+
+        setNewRewardId(randomNum);
+        setRewardBoxId(rewardId);
+
+        setOpenAnimation(true);
+      }
+    } else if (mysteryBoxItem.id === 2) {
+      // held item box
+      randomNum = Math.floor(Math.random() * (16 - 7) + 7);
+
+      const newHeld = held.find((heldItem) => heldItem.id === randomNum);
+
+      setNewRewardPic("images/mysteryBoxGif2.gif");
       setNewRewardName("...");
 
       setTimeout(() => {
-        setNewRewardPic(newCharacter.profile_pic);
-        setNewRewardName(newCharacter.name);
+        setNewRewardPic(newHeld.pic);
+        setNewRewardName(newHeld.name);
       }, 2500);
 
       setNewRewardId(randomNum);
+      setRewardBoxId(rewardId);
+
+      setOpenAnimation(true);
+    } else if (mysteryBoxItem.id === 3) {
+      // consumable box
+      randomNum = Math.floor(Math.random() * 6 + 1);
+
+      const newConsumable = consumables.find(
+        (consumableItem) => consumableItem.id === randomNum
+      );
+
+      setNewRewardPic("images/mysteryBoxGif2.gif");
+      setNewRewardName("...");
+
+      setTimeout(() => {
+        setNewRewardPic(newConsumable.pic);
+        setNewRewardName(newConsumable.name);
+      }, 2500);
+
+      setNewRewardId(randomNum);
+      setRewardBoxId(rewardId);
+
+      setOpenAnimation(true);
+    } else if (mysteryBoxItem.id === 4) {
+      // all item box
+      randomNum = Math.floor(Math.random() * 15 + 1);
+
+      const newItem = allItems.find((item) => item.id === randomNum);
+
+      setNewRewardPic("images/mysteryBoxGif2.gif");
+      setNewRewardName("...");
+
+      setTimeout(() => {
+        setNewRewardPic(newItem.pic);
+        setNewRewardName(newItem.name);
+      }, 2500);
+
+      setNewRewardId(randomNum);
+      setRewardBoxId(rewardId);
 
       setOpenAnimation(true);
     }
+  }
   };
 
   const [openAnimation, setOpenAnimation] = useState(false);
@@ -74,16 +140,58 @@ function MysteryBoxItem({ mysteryBoxItem }) {
   };
 
   const handleCloseAnimation = () => {
-    setRandomOpen(false);
+    if (rewardBoxId === 1) {
+      // character box
+
+      dispatch({
+        type: "SAGA_BUY_NEW_CHARACTER",
+        payload: {
+          characterID: newRewardId,
+          characterCost: 0,
+        },
+      });
+    } else if (rewardBoxId === 2) {
+      // held item box
+
+      dispatch({
+        type: "SAGA_BUY_ITEM",
+        payload: {
+          itemId: newRewardId,
+          amountNum: 1,
+          totalCoins: 0,
+        },
+      });
+    } else if (rewardBoxId === 3) {
+      // consumable box
+
+      dispatch({
+        type: "SAGA_BUY_ITEM",
+        payload: {
+          itemId: newRewardId,
+          amountNum: 1,
+          totalCoins: 0,
+        },
+      });
+    } else if (rewardBoxId === 4) {
+      // all item box
+
+      dispatch({
+        type: "SAGA_BUY_ITEM",
+        payload: {
+          itemId: newRewardId,
+          amountNum: 1,
+          totalCoins: 0,
+        },
+      });
+    }
 
     dispatch({
-      type: "SAGA_BUY_NEW_CHARACTER",
-      payload: {
-        characterID: newRewardId,
-        characterCost: 15,
-      },
+      type: "SAGA_OPEN_BOX",
+      payload: { rewardId: rewardBoxId },
     });
+
     setOpenAnimation(false);
+    setOpenReward(false);
   };
 
   return (
