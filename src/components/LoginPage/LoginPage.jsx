06c32844f28c3cfa214fb753ed84c9a8b4cf90b5
Jacob Larson <hockeybobber@comcast.net>
2024-04-03 18:34:29 -0500

    added header to login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import LoginForm from '../LoginForm/LoginForm';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import LoginForm from "../LoginForm/LoginForm";
+import { useHistory } from "react-router-dom";
 
 function LoginPage() {
   const history = useHistory();
 
   return (
     <>
-    {/* <div className='bowserLogo'></div> */}
-    <div>
+      {/* <div className='bowserLogo'></div> */}
+      <div>
+        <div className="bowsermonMainHeader">
+          <img src="images/bowserLogo2.png" height="150px" width="150px" />
 
-<h1 className='bowsermonMainHeader'>Bowsermon</h1>
+          <h1>Bowsermon</h1>
+        </div>
 
-      <LoginForm />
+        <LoginForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+        <center>
+          <button
+            type="button"
+            className="btn btn_asLink"
+            onClick={() => {
+              history.push("/registration");
+            }}
+          >
+            Register
+          </button>
+        </center>
+      </div>
     </>
   );
 }
