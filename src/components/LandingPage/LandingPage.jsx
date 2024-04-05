a6d3de3120bc74d4afe78c8182529ddd7d13913b
Jacob Larson <hockeybobber@comcast.net>
2024-04-05 17:59:07 -0500

    made it the register form

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import './LandingPage.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./LandingPage.css";
 
 // CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+import RegisterForm from "../RegisterForm/RegisterForm";
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState("Welcome");
   const history = useHistory();
 
   const onLogin = (event) => {
-    history.push('/login');
+    history.push("/login");
   };
 
   return (
-    <div className="container">
-      <h2>{heading}</h2>
+    <div>
+      <div className="bowsermonMainHeader">
+        <img
+          src="images/bowserLogoRedCircle.webp"
+          height="150px"
+          width="150px"
+        />
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-
-        <div className="bowsermonMainHeader">
-        <img src="images/bowserLogoRedCircle.webp" height="150px" width="150px" />
-
-          <h1>Bowsermon</h1>
-        </div>
-        
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a User?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
+        <h1>Bowsermon</h1>
       </div>
+
+      <RegisterForm />
+
+      <center>
+        <button
+          type="button"
+          className="btn btn_asLink"
+          onClick={() => {
+            history.push("/login");
+          }}
+        >
+          Login
+        </button>
+      </center>
     </div>
   );
 }
