b75ad791921b51f881e1ed611c794f379565e429
Jacob Larson <hockeybobber@comcast.net>
2024-04-05 17:52:02 -0500

    formatted

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -10,7 +10,11 @@ function LoginPage() {
       {/* <div className='bowserLogo'></div> */}
       <div>
         <div className="bowsermonMainHeader">
-        <img src="images/bowserLogoRedCircle.webp" height="150px" width="150px" />
+          <img
+            src="images/bowserLogoRedCircle.webp"
+            height="150px"
+            width="150px"
+          />
 
           <h1>Bowsermon</h1>
         </div>
