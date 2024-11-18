af5b737e7f4144347ccd952b3a01e7379aa10715
Jacob Larson <hockeybobber@comcast.net>
2024-11-18 17:51:20 -0600

    added the switch button only pop up when theres 2 starters

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,7 +6,6 @@ import Nav from "../Nav/Nav";
 import StarterItem from "../StarterItem/StarterItem";
 import SwitchStarterButton from "../SwitchStarterButton/SwitchStarterButton";
 
-
 function HomePage() {
   const history = useHistory();
   const dispatch = useDispatch();
@@ -26,28 +25,31 @@ function HomePage() {
     <div>
       <Nav />
 
-      <div className='bowserLogo'></div>
-      
+      <div className="bowserLogo"></div>
+
       <div className="homePage">
-
         <div className="bigStarterHomeView">
+          <div className="switchButtonHomePage">
+            {starter.length === 2 ? <SwitchStarterButton /> : ""}
+          </div>
 
-        <SwitchStarterButton />
-        <div className="starterHomeView">
-
-
-          {starter.map((start) => {
-            return (
-              <div className="starter-single-box" key={start.id}>
-                <h2>
-                  Starter{" "}
-                  {start.starter_1 === true ? "1" : start.starter_2 ? "2" : ""}
-                </h2>
-                <StarterItem key={start.id} start={start} />
-              </div>
-            );
-          })}
-        </div>
+          <div className="starterHomeView">
+            {starter.map((start) => {
+              return (
+                <div className="starter-single-box" key={start.id}>
+                  <h2>
+                    Starter{" "}
+                    {start.starter_1 === true
+                      ? "1"
+                      : start.starter_2
+                      ? "2"
+                      : ""}
+                  </h2>
+                  <StarterItem key={start.id} start={start} />
+                </div>
+              );
+            })}
+          </div>
         </div>
 
         <button
